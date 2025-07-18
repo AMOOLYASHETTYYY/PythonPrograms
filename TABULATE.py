129f48ce188c5c14a2c7c61c2d82179b5f38cd60
AMOOLYASHETTYYY <141319365+AMOOLYASHETTYYY@users.noreply.github.com>
2025-07-18 15:45:10 +0530

    added

--- TABULATE.py
+++ TABULATE.py
@@ -1,11 +1,6 @@
 from tabulate import tabulate
-
-headers = ["name", "age", "department"]
-data = [
-    ["Ravi", 25, "HR"],
-    ["Anjali", 30, "Finance"],
-    ["Mohan", 28, "IT"],
-    ["Sneha", 26, "marketting"],
-    ["Arun", 32, "logistics"]
+headers=["name","age","department"]
+data=[
+    ["Ravi",25,"HR"],
 ]
 print(tabulate(data,headers=headers,tablefmt="fancy_grid"))
